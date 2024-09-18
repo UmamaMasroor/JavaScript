596cb16dbec9d3c0358a2b28c8c36a038d698fc0
UmamaMasroor <129618205+UmamaMasroor@users.noreply.github.com>
2024-09-18 01:35:20 +0000

    variables

---
+++ 01_basics/variables.js
@@ -0,0 +1,21 @@
+let a = 34;
+
+var name = "ali";
+// prefer not use var because of issue in block scope and functional scope
+
+const number = 1234;
+
+user = "ali"
+
+console.log(a, name,number,user);
+
+a = "34";
+
+name = 91;
+
+// number = "true";   // not changeable constant value
+// number = 345       // can't assign another value with same data type
+
+user = 24;
+
+console.log(a, name,number,user);
