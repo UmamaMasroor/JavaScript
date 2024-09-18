13375bb4d1542f1c0ea098f1313d7e4ce0d1620f
UmamaMasroor <129618205+UmamaMasroor@users.noreply.github.com>
2024-09-18 01:47:27 +0000

    data types

---
+++ 01_basics/dataTypes.js
@@ -0,0 +1,22 @@
+"use strict" // for newer version
+
+let string = "Ali";
+
+let number = 22;
+
+let boolean = true;
+let value; // undefined
+
+console.log(typeof(boolean));
+console.log(typeof( value));
+
+// null > standalone value
+let empty = null;
+console.log(typeof(empty)); // null type is object
+console.log(typeof (undefined));   // undefined
+
+
+
+
+
+
